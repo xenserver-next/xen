39323a45e5839e9a7365d17f4ffdb5b340308f7a
Bernhard Kaindl <bernhard.kaindl@citrix.com>
2026-03-05 12:00:00 +0000

    xen/page_alloc: Claims protection: Account unclaimed pages with a claim
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@citrix.com>

--- xen/common/page_alloc.c
+++ xen/common/page_alloc.c
@@ -866,6 +866,29 @@ static void check_and_stop_scrub(struct page_info *head)
     }
 }
 
+/* Answer if memory and claims permit this request to proceed */
+static inline bool protect_outstanding_claims(const struct domain *d,
+                                              unsigned long avail_pages,
+                                              unsigned long claims,
+                                              unsigned int memflags,
+                                              unsigned long request)
+{
+    unsigned long unclaimed_pages;
+
+    ASSERT(spin_is_locked(&heap_lock));
+    ASSERT(avail_pages >= claims);
+    unclaimed_pages = avail_pages - claims;
+
+    if ( unclaimed_pages >= request ) /* Enough unclaimed pages? */
+        return true;
+
+    /* Else, we need a domain (with claims) and we need to be refcounting */
+    if ( !d || (memflags & MEMF_no_refcount) )
+        return false;
+
+    return request <= unclaimed_pages + d->outstanding_pages;
+}
+
 static struct page_info *get_free_buddy(unsigned int zone_lo,
                                         unsigned int zone_hi,
                                         unsigned int order, unsigned int memflags,
@@ -1023,9 +1046,8 @@ static struct page_info *alloc_heap_pages(
      * Claimed memory is considered unavailable unless the request
      * is made by a domain with sufficient unclaimed pages.
      */
-    if ( (outstanding_claims + request > total_avail_pages) &&
-          ((memflags & MEMF_no_refcount) ||
-           !d || d->outstanding_pages < request) )
+    if ( !protect_outstanding_claims(d, total_avail_pages, outstanding_claims,
+                                     memflags, request) )
     {
         spin_unlock(&heap_lock);
         return NULL;
