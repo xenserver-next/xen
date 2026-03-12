xenserver-next/xen

commit b43abf5ca3412554b97936e57714581c86ff440f

mm: Keep heap accessible to others while scrubbing
Instead of scrubbing pages while holding heap lock we can mark
buddy's head as being scrubbed and drop the lock temporarily.
If someone (most likely alloc_heap_pages()) tries to access
this chunk it will signal the scrubber to abort scrub by setting
head's BUDDY_SCRUB_ABORT bit. The scrubber checks this bit after
processing each page and stops its work as soon as it sees it.

Signed-off-by: Boris Ostrovsky <boris.ostrovsky@oracle.com>
Reviewed-by: Jan Beulich <jbeulich@suse.com>
Acked-by: Julien Grall <julien.grall@arm.com>

xen/common/page_alloc.c
@@ -683,6 +683,7 @@ static void page_list_add_scrub(struct page_info *pg, unsigned int node,
 {
     PFN_ORDER(pg) = order;
     pg->u.free.first_dirty = first_dirty;
+    pg->u.free.scrub_state = BUDDY_NOT_SCRUBBING;
 
     if ( first_dirty != INVALID_DIRTY_IDX )
     {
@@ -691,6 +692,25 @@ static void page_list_add_scrub(struct page_info *pg, unsigned int node,
     }
     else
         page_list_add(pg, &heap(node, zone, order));
+}
+
+static void check_and_stop_scrub(struct page_info *head)
+{
+    if ( head->u.free.scrub_state == BUDDY_SCRUBBING )
+    {
+        typeof(head->u.free) pgfree;
+
+        head->u.free.scrub_state = BUDDY_SCRUB_ABORT;
+        spin_lock_kick();
+        for ( ; ; )
+        {
+            /* Can't ACCESS_ONCE() a bitfield. */
+            pgfree.val = ACCESS_ONCE(head->u.free.val);
+            if ( pgfree.scrub_state != BUDDY_SCRUB_ABORT )
+                break;
+            cpu_relax();
+        }
+    }
 }
 
 static struct page_info *get_free_buddy(unsigned int zone_lo,
@@ -737,14 +757,19 @@ static struct page_info *get_free_buddy(unsigned int zone_lo,
             {
                 if ( (pg = page_list_remove_head(&heap(node, zone, j))) )
                 {
+                    if ( pg->u.free.first_dirty == INVALID_DIRTY_IDX )
+                        return pg;
                     /*
                      * We grab single pages (order=0) even if they are
                      * unscrubbed. Given that scrubbing one page is fairly quick
                      * it is not worth breaking higher orders.
                      */
-                    if ( (order == 0) || use_unscrubbed ||
-                         pg->u.free.first_dirty == INVALID_DIRTY_IDX)
+                    if ( (order == 0) || use_unscrubbed )
+                    {
+                        check_and_stop_scrub(pg);
                         return pg;
+                    }
+
                     page_list_add_tail(pg, &heap(node, zone, j));
                 }
             }
@@ -925,6 +950,7 @@ static int reserve_offlined_page(struct page_info *head)
 
     cur_head = head;
 
+    check_and_stop_scrub(head);
     /*
      * We may break the buddy so let's mark the head as clean. Then, when
      * merging chunks back into the heap, we will see whether the chunk has
@@ -1075,6 +1101,29 @@ static unsigned int node_to_scrub(bool get_node)
     return closest;
 }
 
+struct scrub_wait_state {
+    struct page_info *pg;
+    unsigned int first_dirty;
+    bool drop;
+};
+
+static void scrub_continue(void *data)
+{
+    struct scrub_wait_state *st = data;
+
+    if ( st->drop )
+        return;
+
+    if ( st->pg->u.free.scrub_state == BUDDY_SCRUB_ABORT )
+    {
+        /* There is a waiter for this buddy. Release it. */
+        st->drop = true;
+        st->pg->u.free.first_dirty = st->first_dirty;
+        smp_wmb();
+        st->pg->u.free.scrub_state = BUDDY_NOT_SCRUBBING;
+    }
+}
+
 bool scrub_free_pages(void)
 {
     struct page_info *pg;
@@ -1097,24 +1146,52 @@ bool scrub_free_pages(void)
         do {
             while ( !page_list_empty(&heap(node, zone, order)) )
             {
-                unsigned int i;
+                unsigned int i, dirty_cnt;
+                struct scrub_wait_state st;
 
                 /* Unscrubbed pages are always at the end of the list. */
                 pg = page_list_last(&heap(node, zone, order));
                 if ( pg->u.free.first_dirty == INVALID_DIRTY_IDX )
                     break;
 
+                ASSERT(pg->u.free.scrub_state == BUDDY_NOT_SCRUBBING);
+                pg->u.free.scrub_state = BUDDY_SCRUBBING;
+
+                spin_unlock(&heap_lock);
+
+                dirty_cnt = 0;
+
                 for ( i = pg->u.free.first_dirty; i < (1U << order); i++)
                 {
                     if ( test_bit(_PGC_need_scrub, &pg[i].count_info) )
                     {
                         scrub_one_page(&pg[i]);
+                        /*
+                         * We can modify count_info without holding heap
+                         * lock since we effectively locked this buddy by
+                         * setting its scrub_state.
+                         */
                         pg[i].count_info &= ~PGC_need_scrub;
-                        node_need_scrub[node]--;
+                        dirty_cnt++;
                         cnt += 100; /* scrubbed pages add heavier weight. */
                     }
                     else
                         cnt++;
+
+                    if ( pg->u.free.scrub_state == BUDDY_SCRUB_ABORT )
+                    {
+                        /* Someone wants this chunk. Drop everything. */
+
+                        pg->u.free.first_dirty = (i == (1U << order) - 1) ?
+                            INVALID_DIRTY_IDX : i + 1; 
+                        smp_wmb();
+                        pg->u.free.scrub_state = BUDDY_NOT_SCRUBBING;
+
+                        spin_lock(&heap_lock);
+                        node_need_scrub[node] -= dirty_cnt;
+                        spin_unlock(&heap_lock);
+                        goto out_nolock;
+                    }
 
                     /*
                      * Scrub a few (8) pages before becoming eligible for
@@ -1129,6 +1206,23 @@ bool scrub_free_pages(void)
                     }
                 }
 
+                st.pg = pg;
+                /*
+                 * get_free_buddy() grabs a buddy with first_dirty set to
+                 * INVALID_DIRTY_IDX so we can't set pg's first_dirty here.
+                 * It will be set either below or in the lock callback (in
+                 * scrub_continue()).
+                 */
+                st.first_dirty = (i >= (1U << order) - 1) ?
+                    INVALID_DIRTY_IDX : i + 1;
+                st.drop = false;
+                spin_lock_cb(&heap_lock, scrub_continue, &st);
+
+                node_need_scrub[node] -= dirty_cnt;
+
+                if ( st.drop )
+                    goto out;
+
                 if ( i >= (1U << order) - 1 )
                 {
                     page_list_del(pg, &heap(node, zone, order));
@@ -1136,6 +1230,8 @@ bool scrub_free_pages(void)
                 }
                 else
                     pg->u.free.first_dirty = i + 1;
+
+                pg->u.free.scrub_state = BUDDY_NOT_SCRUBBING;
 
                 if ( preempt || (node_need_scrub[node] == 0) )
                     goto out;
@@ -1145,6 +1241,8 @@ bool scrub_free_pages(void)
 
  out:
     spin_unlock(&heap_lock);
+
+ out_nolock:
     node_clear(node, node_scrubbing);
     return node_to_scrub(false) != NUMA_NO_NODE;
 }
@@ -1226,6 +1324,8 @@ static void free_heap_pages(
                  (phys_to_nid(page_to_maddr(predecessor)) != node) )
                 break;
 
+            check_and_stop_scrub(predecessor);
+
             page_list_del(predecessor, &heap(node, zone, order));
 
             /* Keep predecessor's first_dirty if it is already set. */
@@ -1246,6 +1346,8 @@ static void free_heap_pages(
                  (PFN_ORDER(successor) != order) ||
                  (phys_to_nid(page_to_maddr(successor)) != node) )
                 break;
+
+            check_and_stop_scrub(successor);
 
             page_list_del(successor, &heap(node, zone, order));
         }

xen/include/asm-arm/mm.h
@@ -42,18 +42,27 @@ struct page_info
             unsigned long type_info;
         } inuse;
         /* Page is on a free list: ((count_info & PGC_count_mask) == 0). */
-        struct {
-            /*
-             * Index of the first *possibly* unscrubbed page in the buddy.
-             * One more bit than maximum possible order to accommodate
-             * INVALID_DIRTY_IDX.
-             */
+        union {
+            struct {
+                /*
+                 * Index of the first *possibly* unscrubbed page in the buddy.
+                 * One more bit than maximum possible order to accommodate
+                 * INVALID_DIRTY_IDX.
+                 */
 #define INVALID_DIRTY_IDX ((1UL << (MAX_ORDER + 1)) - 1)
-            unsigned long first_dirty:MAX_ORDER + 1;
+                unsigned long first_dirty:MAX_ORDER + 1;
 
-            /* Do TLBs need flushing for safety before next page use? */
-            bool need_tlbflush:1;
-        } free;
+                /* Do TLBs need flushing for safety before next page use? */
+                bool need_tlbflush:1;
+
+#define BUDDY_NOT_SCRUBBING    0
+#define BUDDY_SCRUBBING        1
+#define BUDDY_SCRUB_ABORT      2
+                unsigned long scrub_state:2;
+            };
+
+            unsigned long val;
+            } free;
 
     } u;
 

xen/include/asm-x86/mm.h
@@ -86,17 +86,26 @@ struct page_info
         } sh;
 
         /* Page is on a free list: ((count_info & PGC_count_mask) == 0). */
-        struct {
-            /*
-             * Index of the first *possibly* unscrubbed page in the buddy.
-             * One more bit than maximum possible order to accommodate
-             * INVALID_DIRTY_IDX.
-             */
+        union {
+            struct {
+                /*
+                 * Index of the first *possibly* unscrubbed page in the buddy.
+                 * One more bit than maximum possible order to accommodate
+                 * INVALID_DIRTY_IDX.
+                 */
 #define INVALID_DIRTY_IDX ((1UL << (MAX_ORDER + 1)) - 1)
-            unsigned int first_dirty;
+                unsigned int first_dirty;
 
-            /* Do TLBs need flushing for safety before next page use? */
-            bool_t need_tlbflush;
+                /* Do TLBs need flushing for safety before next page use? */
+                bool need_tlbflush;
+
+#define BUDDY_NOT_SCRUBBING    0
+#define BUDDY_SCRUBBING        1
+#define BUDDY_SCRUB_ABORT      2
+                uint8_t  scrub_state;
+            };
+
+            unsigned long val;
         } free;
 
     } u;

commit 153fd3cce10153ca0e75a48e9ed66cbc7be3dd25

xen/page_alloc: Idle scrubber: Use atomic bit clear for PGC_need_scrub
scrub_free_pages() clears PGC_need_scrub with a plain non-atomic
read-modify-write while not holding heap_lock:

    if ( test_bit(_PGC_need_scrub, &pg[i].count_info) )
    {
        scrub_one_page(&pg[i], true);
        /*
         * We can modify count_info without holding heap
         * lock since we effectively locked this buddy by
         * setting its scrub_state.
         */
        pg[i].count_info &= ~PGC_need_scrub;

But scrub_state only prevents the buddy from being allocated or removed
from the free list. It does not serialise changes to pg[i].count_info:

Step 1 - CPU A: scrub_free_pages() fetches pg[i].count_info after scrubbing
Step 2 - CPU B: mark_page_offline() does cmpxchg of count_info
Step 3 - CPU A: scrub_free_pages() writes count_info after &= ~PGC_need_scrub;

Outcome: The change of step 2 is lost. A page just scrubbed may fail to
become offlined if mark_page_offline() was racing with the status update.

It could even cause xc_mark_page_offline() to report success although the
page was not offlined (depending on when the update becomes visible).

Fix: alloc_heap_pages() and populate_physmap() already avoid the
same issue using test_and_clear_bit(). Use the same pattern:

        if ( test_and_clear_bit(_PGC_need_scrub, &pg[i].count_info) )
            scrub_one_page(&pg[i], true);

This clears _PGC_need_scrub before scrubbing, like alloc_heap_pages() and
populate_physmap() already do. _PGC_need_scrub is only used to flag if
a page needs scrubbing and is not used for other purposes:

- When the scrub pass is aborted, any in-progress scrub_one_page()
  call still complete; the abort is handled after the loop.

- Clearing _PGC_need_scrub here is safe: we will always scrub the page
  (except for PGC_broken pages, which scrub_one_page() skips).

Effects:

- For tools, tests, services which may offline pages: When the returned
  status indicated that the page was offlined, it was indeed offlined.

- When MCEs report pages as needing offlining, this fix ensures that
  the pages which shall be offlined are indeed offlined and will not
  remain available for allocation due to a race condition.

Fixes: b43abf5ca341 ("mm: Keep heap accessible to others while scrubbing")

xen/common/page_alloc.c
@@ -1378,15 +1378,17 @@ bool scrub_free_pages(void)
 
                 for ( i = pg->u.free.first_dirty; i < (1U << order); i++)
                 {
-                    if ( test_bit(_PGC_need_scrub, &pg[i].count_info) )
+                    /*
+                     * scrub_state prevents removal, but not state updates.
+                     * mark_page_offline() can set a page offlined at any time,
+                     * including concurrently with this loop. It uses cmpxchg
+                     * to set the offlined state bits, so we use the same
+                     * pattern as alloc_heap_pages() and populate_physmap():
+                     */
+                    if ( test_and_clear_bit(_PGC_need_scrub,
+                                            &pg[i].count_info) )
                     {
                         scrub_one_page(&pg[i], true);
-                        /*
-                         * We can modify count_info without holding heap
-                         * lock since we effectively locked this buddy by
-                         * setting its scrub_state.
-                         */
-                        pg[i].count_info &= ~PGC_need_scrub;
                         dirty_cnt++;
                         cnt += 100; /* scrubbed pages add heavier weight. */
                     }