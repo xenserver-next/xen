c155a7966bf4ad54c4f9cb48b8e1fd13539b0200
Bernhard Kaindl <bernhard.kaindl@citrix.com>
2026-04-29 11:00:00 +0200

    tools/ocaml/libs/xc: Add an OCaml binding for NUMA-aware claims
    Add an OCaml binding for xc_domain_claim_memory() so the
    XEN_DOMCTL_claim_memory hypercall can be used from OCaml.
    
    This allows OCaml toolstacks to place NUMA-aware memory
    claims for domains as well as host-wide claims.
    
    tools/ocaml/libs/xc/xenctrl.ml/mli:
    - Add claim record type and domain_claim_memory external.
    
    tools/ocaml/libs/xc/xenctrl_stubs.c:
    - Validate claim count and arguments.
    - Marshal the OCaml claim array to memory_claim_t[].
    - Map node of -1 to XEN_DOMCTL_CLAIM_MEMORY_GLOBAL.
    
    Acked-by: Christian Lindig <christian.lindig@cloud.com>
    Requested-by: Andrew Cooper <andrew.cooper3@citrix.com>
    Signed-off-by: Bernhard Kaindl <bernhard.kaindl@citrix.com>

--- tools/ocaml/libs/xc/xenctrl.mli
+++ tools/ocaml/libs/xc/xenctrl.mli
@@ -297,6 +297,17 @@ external domain_deassign_device: handle -> domid -> (int * int * int * int) -> u
 external domain_test_assign_device: handle -> domid -> (int * int * int * int) -> bool
   = "stub_xc_domain_test_assign_device"
 
+(* OCaml binding for xc_domain_claim_memory(): Install claim sets for a domain,
+   target is either a NUMA node or -1 for no node - memory from any node. *)
+
+type claim =
+  {
+    pages: int64; (* Number of pages to claim *)
+    node: int32;  (* NUMA node ID, or -1 for a floating claim *)
+  }
+external domain_claim_memory: handle -> domid -> claim array -> unit
+  = "stub_xc_domain_claim_memory"
+
 external version : handle -> version = "stub_xc_version_version"
 external version_compile_info : handle -> compile_info
   = "stub_xc_version_compile_info"
